8e73fc5d48d2500b0aa99b63fc51de0df5a485b4
Brady Olsen <brady@tangocard.com>
2017-11-01 18:23:22 -0700

    add swagger codegen language configs and update generation

--- generate-sdks.sh
+++ generate-sdks.sh
@@ -2,13 +2,26 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+REPOS_PATH=$DIR/..
+SWAGGER_CODEGEN_PATH=$REPOS_PATH/aces-swagger-codegen/modules/swagger-codegen-cli/target
+ENCODED_LISTENER_API_PATH=$REPOS_PATH/aces-encoded-listener-api
+SERVICE_API_PATH=$REPOS_PATH/aces-service-api
+
+
 for LANGUAGE in java php go javascript python csharp
 do
-    rm -rf $DIR/../aces-encoded-listener-api/sdks/$LANGUAGE/
-	java -jar $DIR/../aces-swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-       -i $DIR/../aces-encoded-listener-api/aces-encoded-listener-api-swagger.yaml \
-       -l $LANGUAGE \
-       -o $DIR/../aces-encoded-listener-api/sdks/$LANGUAGE/ \
-       -c $DIR/sdk-configs/$LANGUAGE.json
+    rm -rf $ENCODED_LISTENER_API_PATH/sdks/$LANGUAGE/
+    java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar generate \
+        -i $ENCODED_LISTENER_API_PATH/aces-encoded-listener-api-swagger.yaml \
+        -l $LANGUAGE \
+        -o $ENCODED_LISTENER_API_PATH/sdks/$LANGUAGE/ \
+        -c $DIR/configs/encoded-listener-sdks/$LANGUAGE.json
+
+    rm -rf $SERVICE_API_PATH/sdks/$LANGUAGE/
+    java -jar $SWAGGER_CODEGEN_PATH/swagger-codegen-cli.jar generate \
+        -i $SERVICE_API_PATH/aces-service-api-swagger.yaml \
+        -l $LANGUAGE \
+        -o $SERVICE_API_PATH/sdks/$LANGUAGE/ \
+        -c $DIR/configs/service-sdks/$LANGUAGE.json
 done
 
